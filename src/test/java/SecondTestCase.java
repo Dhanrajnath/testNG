68a39293c134a2faf6d13699314ba5c4e4a3a41f
Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2023-04-25 12:25:17 +0530

    Second testcase

---
+++ src/test/java/SecondTestCase.java
@@ -0,0 +1,25 @@
+import org.testng.annotations.Test;
+
+public class SecondTestCase {
+
+    @Test(priority = 1)
+    void setup(){
+        System.out.println("TEST2:opening browser");
+    }
+
+    @Test(priority = 3)
+    void searchCustomer(){
+        System.out.println("TEST2:This is search customer test");
+    }
+
+    @Test(priority = 2)
+    void addCustomer(){
+        System.out.println("TEST2:This is add customer test");
+    }
+
+    @Test(priority = 4)
+    void teardown(){
+        System.out.println("TEST2:closing browser");
+    }
+
+}
