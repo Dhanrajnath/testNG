4dd49cbed6dfbfb73b312e00e6027e013cf6c2d9
Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2023-04-25 13:37:25 +0530

    adding testcases to suite & viewing test report

--- src/test/java/SecondTestCase.java
+++ src/test/java/SecondTestCase.java
@@ -1,24 +1,26 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SecondTestCase {
 
     @Test(priority = 1)
-    void setup(){
+    void setup() {
         System.out.println("TEST2:opening browser");
     }
 
     @Test(priority = 3)
-    void searchCustomer(){
+    void searchCustomer() {
         System.out.println("TEST2:This is search customer test");
+//        Assert.assertEquals(1, 2 - 2);
     }
 
     @Test(priority = 2)
-    void addCustomer(){
+    void addCustomer() {
         System.out.println("TEST2:This is add customer test");
     }
 
     @Test(priority = 4)
-    void teardown(){
+    void teardown() {
         System.out.println("TEST2:closing browser");
     }
 
