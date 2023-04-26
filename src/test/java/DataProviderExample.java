0cc359ce57b44a07a624794c019365398ac357b5
Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2023-04-26 13:31:08 +0530

    data provider in different class & dataProviderClass arg

--- src/test/java/DataProviderExample.java
+++ src/test/java/DataProviderExample.java
@@ -1,19 +1,13 @@
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DataProviderExample {
 
-    @Test(dataProvider = "LoginDataProvider")
+    @Test(dataProvider = "LoginDataProvider", dataProviderClass = CustomDataProvider.class)
     public void loginTest(String email, String password) {
         System.out.println("DataProviderExample.loginTest");
         System.out.println("email = " + email + ", password = " + password);
         Assert.assertEquals(email.split("@")[0], password);
     }
 
-    @DataProvider(name = "LoginDataProvider")
-    public Object[][] getData() {
-        Object[][] data = {{"abc@gmail.com", "abc"}, {"xyz@gmail.com", "xyz"}};
-        return data;
-    }
 }
