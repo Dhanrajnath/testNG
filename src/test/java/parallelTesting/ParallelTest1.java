8f1f8dfafa49349ab6abaf851240327150b4f083
Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2023-04-27 11:48:27 +0530

    parallel testing in testNG --> methods, classes & tests

---
+++ src/test/java/parallelTesting/ParallelTest1.java
@@ -0,0 +1,16 @@
+package parallelTesting;
+
+import org.testng.annotations.Test;
+
+public class ParallelTest1 {
+
+    @Test
+    public void testLogo() {
+        System.out.println("ParallelTest1.testLogo");
+    }
+
+    @Test
+    public void testTitle() {
+        System.out.println("ParallelTest1.testTitle");
+    }
+}
