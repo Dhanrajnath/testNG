210fde2786c974c9b418d81d79bb29a98b56be3b
Dhanrajnath <dhanrajnath.porika@zemosolabs.com>
2023-04-27 14:08:21 +0530

    matching test method name with regex pattern

---
+++ src/test/java/RegexTest.java
@@ -0,0 +1,25 @@
+import org.testng.annotations.Test;
+
+public class RegexTest {
+
+    @Test
+    public void testOne() {
+        System.out.println("RegexTest.testOne");
+    }
+
+    @Test
+    public void testTwo() {
+        System.out.println("RegexTest.testTwo");
+    }
+
+    @Test
+    public void oneTest() {
+        System.out.println("RegexTest.oneTest");
+    }
+
+    @Test
+    public void twoTest() {
+        System.out.println("RegexTest.twoTest");
+    }
+
+}
